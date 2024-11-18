ca922eb358dfb02af8f063a067cf20b85238b4fe
João Pedro de Oliveira Martins <104173377+jpp09@users.noreply.github.com>
2024-11-18 03:14:14 +0000

    Realizadno os jauste do front com o backend

--- docs/funcionalidade/add.js
+++ docs/funcionalidade/add.js
@@ -1,8 +1,14 @@
 const form = document.getElementById('form_add')
-form.addEventListener('submit', pegar_dados)
+form.addEventListener('submit', (ev) =>{
+pegar_dados()
+})
 
-
-function pegar_dados(e){
-    e.preventDeafult
-    alert('Deu certo')
+function pegar_dados(){
+    const nome_titulo =  document.querySelector('#nome_filme')
+    const titulo = document.querySelector('#input_title')
+    const spoiler = document.querySelector('input[name="input_spoiler"]:checked')
+    const categoria = document.querySelector('select[name="categoria"]')
+    nome_titulo.value = ''
+    titulo.value = ''
+    spoiler.value = ''
 }

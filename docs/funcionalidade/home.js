30788fb5e62d30062552568eed310b067641396e
João Pedro de Oliveira Martins <104173377+jpp09@users.noreply.github.com>
2024-11-23 12:30:07 +0000

    Fazendo a conexão do backend com o front

--- docs/funcionalidade/home.js
+++ docs/funcionalidade/home.js
@@ -1,15 +1,13 @@
-const url = 'https://api.themoviedb.org/3/trending/movie/day?language=en-US';
-const options = {
-  method: 'GET',
-  headers: {
-    accept: 'application/json',
-    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmODZmNmQ2YzZkNTVhODJiNmY5MmU4NWE0ODc0MTljYyIsIm5iZiI6MTczMjEyMTY4Ny42NjE2NDEsInN1YiI6IjY3MmU5M2U4N2ZkNzI0MzQyYTkwMDNhNyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.ZPOxVFX4Vknfx33ERuTOL4Hi49EMDLx6t9RI6cZnWfI'
-  }
-};
+const url = 'https://trabalhofaculdadebackend.onrender.com/home'
 
-const teste = fetch(url, options)
-  .then(res => res.json())
-  .then(json => json.results.forEach(element => criar(element.overview,element.poster_path)))
+const pegarConteudo = fetch(url,{
+  headers: {
+    'Content-Type': 'application/json',
+  },
+  method: 'get'
+})
+.then(res => res.json())
+.then(json => json.results.forEach(element => criar(element.overview,element.poster_path)))
 
 function criar(ov,img){
     const p = document.createElement('p')

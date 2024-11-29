538941f7d10538f2f6465594d0219e13aaedbcb7
João Pedro de Oliveira Martins <104173377+jpp09@users.noreply.github.com>
2024-11-29 21:31:49 +0000

    Arrumando o bt de logout

--- docs/funcionalidade/verificar_log_in.js
+++ docs/funcionalidade/verificar_log_in.js
@@ -39,7 +39,7 @@ function check_login(log){
   }
 
 logout.addEventListener('click',  deslogar)
-logout_drop.addEventeListener('click',deslogar)
+logout_drop.addEventListener('click',deslogar)
 
 function deslogar(){
   fetch(url_deslogar,{headers:{
